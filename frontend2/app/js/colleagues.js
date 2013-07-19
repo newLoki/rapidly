79256661016567d9c850f90891d83e7040472cf0
Michael Kleinschnitz <michael.kleinschnitz@mayflower.de>
2013-07-19 14:11:39 +0200

    delete from list

--- frontend2/app/js/colleagues.js
+++ frontend2/app/js/colleagues.js
@@ -1,12 +1,23 @@
 function ColleaguesController($scope) {
     $scope.colleagues = [
-      {id: 1, name: "Michael"},
-      {id: 2, name: "Norbert"},
-      {id: 3, name: "Peter"}
+        {id: 1, name: "Michael"},
+        {id: 2, name: "Norbert"},
+        {id: 3, name: "Peter"}
     ];
 
-    $scope.addColleague = function() {
-      $scope.colleagues.push({name:$scope.colleagueName});
-      $scope.colleagueName = '';
+    $scope.addColleague = function () {
+        $scope.colleagues.push({name: $scope.colleagueName});
+        $scope.colleagueName = '';
     };
+
+    $scope.deleteColleague = function (colleagueToDelete) {
+        var oldList = $scope.colleagues;
+        $scope.colleagues = [];
+
+        angular.forEach(oldList, function (colleague) {
+            if (colleague.$$hashKey != colleagueToDelete.$$hashKey) {
+                $scope.colleagues.push(colleague);
+            }
+        });
+    }
 }
